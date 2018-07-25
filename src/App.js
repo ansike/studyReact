d2f5f46db52f91013b7cc580b59be3015db79094
ansike <984315765@qq.com>
2018-07-25 09:32:28 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return <div className="App"><img className="App-logo" src={logo} /></div>;
+  }
+}
+
+export default App;
