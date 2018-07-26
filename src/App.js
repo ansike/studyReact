d917e107cb55a8e124bf4436aeb7aaf7bafafa13
ansike <984315765@qq.com>
2018-07-26 09:32:20 +0800

    添加路由文件

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Top from "./components/top";
+import Routers from "./router";
 class App extends Component {
   render() {
     return (
     <div className="App">
-      <img className="App-logo" alt="logo" src={logo} />
-      <Top />
+        <img className="App-logo" alt="logo" src={logo} />
+        <Top />
+        <Routers />
     </div>
     )
   }
--- src/router.js
+++ src/router.js
@@ -1,2 +1,23 @@
 import React,{Component} from 'react'
-import { hashHistory, Route, Router } from "react-router";
+// 首先我们需要导入一些组件...
+import { Switch,Router, Route, Link } from 'react-router'
+
+import Home from './components/home';
+import About from './components/about';
+import Inbox from './components/inbox';
+
+export default class Routers extends Component{
+
+    render(){
+        return (
+            <Router>
+                <Switch>
+                <Route path="/" component={Home}>
+                    <Route path="about" component={About} />
+                    <Route path="inbox" component={Inbox} />
+                </Route>
+                </Switch>
+            </Router>
+        )
+    }
+}
